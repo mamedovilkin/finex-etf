7d193611bd7e9d27aca21a6c2268e3ff001e5df5
Ilkin Mamedov <ilkinmamedov0208@gmail.com>
2025-01-16 14:53:14 +0300

    Added localization to introduction

--- app/src/main/java/io/github/mamedovilkin/finexetf/viewmodel/blog/BlogViewModel.kt
+++ app/src/main/java/io/github/mamedovilkin/finexetf/viewmodel/blog/BlogViewModel.kt
@@ -1,11 +1,14 @@
 package io.github.mamedovilkin.finexetf.viewmodel.blog
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.mamedovilkin.network.model.blog.Posts
 import io.github.mamedovilkin.finexetf.repository.UseCase
+import io.github.mamedovilkin.network.model.blog.Meta
+import io.github.mamedovilkin.network.model.blog.Pagination
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
@@ -22,10 +25,20 @@ class BlogViewModel @Inject constructor(
     fun getPosts(page: Int): LiveData<Posts> {
         return liveData {
             _loadingState.value = true
-            useCase.getPosts(page).body()?.let {
-                emit(it)
+            try {
+                val posts = useCase.getPosts(page)
+                if (posts.isSuccessful) {
+                    posts.body()?.let {
+                        emit(it)
+                    }
+                } else {
+                    emit(Posts(emptyList(), Meta(Pagination(0, 0, 0, 0))))
+                    return@liveData
+                }
+                _loadingState.value = false
+            } catch (e: Exception) {
+                Log.e("BlogViewModel", e.message.toString())
             }
-            _loadingState.value = false
         }
     }
 }
